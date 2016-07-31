0ff30e8be7799f1938f4f1469d9a487b5cbd6ebf
Ravi Tyagi <ravityagi@xebia-macbook-pro.local>
2016-07-31 11:21:34 +0530

    Initial Commit

---
+++ GenericsInPlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenericsInPlay
+//
+//  Created by Ravi Tyagi on 31/07/16.
+//  Copyright © 2016 Ravi Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
